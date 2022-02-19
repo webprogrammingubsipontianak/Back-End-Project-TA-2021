3228cd4bf7c3599ee608537c2162c47199478b10
webprogrammingubsipontianak <wpubsipontianak@gmail.com>
2022-02-19 21:49:38 +0700

    upload project back-end tugas akhir saya tahun 2021

---
+++ app/Http/Controllers/API/CheckoutController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\API\CheckoutRequest;
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\TransactionDetail;
+use illuminate\Http\Request;
+
+
+
+class CheckoutController extends Controller
+{
+    public function checkout(CheckoutRequest $request)
+    {
+        // var_dump('testing');
+        // die;
+        $data = $request->except('transaction_details');
+        $data['uuid'] = 'TRX' . mt_rand(10000, 99999) . mt_rand(100, 999);
+
+        $transaction = Transaction::create($data);
+
+        foreach ($request->transaction_details as $product) {
+            $details[] = new TransactionDetail([
+                'transactions_id' => $transaction->id,
+                'products_id' => $product,
+            ]);
+
+            Product::find($product)->decrement('quantity');
+        }
+
+        $transaction->details()->saveMany($details);
+
+        return ResponseFormatter::success($transaction);
+    }
+}
